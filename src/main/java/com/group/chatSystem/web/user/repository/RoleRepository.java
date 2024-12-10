dbcfa70b5cb983c547c9a60117cf0358650dfce5
PJH447 <bopi44777@gmail.com>
2024-12-10 12:22:48 +0900

    feat roleRepo

---
+++ src/main/java/com/group/chatSystem/web/user/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.group.chatSystem.web.user.repository;
+
+import com.group.chatSystem.web.user.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
