d1dd9e22cee6921f4d06bccd7d1fd6ff0b4b184f
PJH447 <bopi44777@gmail.com>
2024-12-10 12:25:58 +0900

    feat signUp

--- src/main/java/com/group/chatSystem/web/user/controller/UserRestController.java
+++ src/main/java/com/group/chatSystem/web/user/controller/UserRestController.java
@@ -1,12 +1,24 @@
 package com.group.chatSystem.web.user.controller;
 
+import com.group.chatSystem.web.common.dto.CommonResponse;
+import com.group.chatSystem.web.user.dto.SignUpForm;
+import com.group.chatSystem.web.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserRestController {
+
+    private final UserService userService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/v1/signUp")
+    public CommonResponse signUp(@RequestBody SignUpForm signUpForm) {
+        userService.signUp(signUpForm);
+        return CommonResponse.createVoidResponse();
+    }
 
 }
