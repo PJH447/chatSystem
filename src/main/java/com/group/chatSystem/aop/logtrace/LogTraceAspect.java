67aac9bc08b659634f1bab225708ccf1a5595873
PJH447 <bopi44777@gmail.com>
2024-12-10 10:48:56 +0900

    feat logTrace - aop

---
+++ src/main/java/com/group/chatSystem/aop/logtrace/LogTraceAspect.java
@@ -0,0 +1,41 @@
+package com.group.chatSystem.aop.logtrace;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Aspect
+@Profile("development")
+public class LogTraceAspect {
+
+    private final LogTrace logTrace;
+
+    public LogTraceAspect(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    @Around("execution(* com.group.chatSystem.web..*Controller*.*(..)) || " +
+            "execution(* com.group.chatSystem.web..*Service*.*(..)) || " +
+            "execution(* com.group.chatSystem.web..*Repository*.*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        TraceStatus status = null;
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+
+            Object result = joinPoint.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+
+}
