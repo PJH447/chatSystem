2a389d3c9f46a17465be7f1a3796a8dc267fb354
PJH447 <bopi44777@gmail.com>
2024-12-10 11:01:33 +0900

    feat webConfig

---
+++ src/main/java/com/group/chatSystem/config/common/WebConfig.java
@@ -0,0 +1,27 @@
+package com.group.chatSystem.config.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .exposedHeaders("header1", "header2", "Authorization", "Set-Cookie")
+                .allowedHeaders("*")
+                .allowCredentials(true).maxAge(3600);
+
+    }
+}
