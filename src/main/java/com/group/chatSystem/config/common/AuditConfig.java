1265e7fd72412b41b5b54eabd7be52b2708044e9
PJH447 <bopi44777@gmail.com>
2024-12-10 10:46:15 +0900

    feat auditConfig

---
+++ src/main/java/com/group/chatSystem/config/common/AuditConfig.java
@@ -0,0 +1,9 @@
+package com.group.chatSystem.config.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class AuditConfig {
+}
